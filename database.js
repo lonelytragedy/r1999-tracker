64967c3ccd81e6f588d9e9a5eed088feb8ad25b3
lonelytragedy <xcuz.me@yandex.ru>
2026-06-16 23:22:05 +0500

    update database.js

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const BANNERS = {
+  "晴日决胜局": { name: "Clash 'n' Slash", type: "Character" },
   "零重力漫游": { name: "Wirewalking in Zero Gravity", type: "Character"},
   "ramona": { name: "Conduit of the Unseen", type: "Character" },
   "奇异吸引子": { name: "The Strange Attractor", type: "Character" },
@@ -30,7 +31,14 @@ const BANNERS = {
 };
 
 const ACTIVE_BANNERS = [
-      {
+  {
+    key:      "晴日决胜局",
+    startUTC: "2026-06-16T10:00:00Z",
+    endUTC:   "2026-06-30T09:59:00Z",
+    image:    "static/banners/Clash_n_Slash.webp",
+    rateUp:  ["J", "Sweetheart", "Click"]
+  },
+  {
     key:      "零重力漫游",
     startUTC: "2026-06-02T10:00:00Z",
     endUTC:   "2026-06-16T09:59:00Z",
